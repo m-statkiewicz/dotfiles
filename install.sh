570ab94b96e9ed234cadfb93db40a8bf9d9861bf
statek <mat.statkiewicz@gmail.com>
2024-01-12 00:05:45 +0000

    ZSH setup

--- install.sh
+++ install.sh
@@ -12,3 +12,13 @@ ln -s "$HOME/dotfiles/X11" "$HOME/.config"
 # i3 #
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+
+# zsh #
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
+
+rm -rf "$HOME/.config/zsh/external"
+ln -sf "$HOME/dotfiles/zsh/external" "$HOME/.config/zsh"
+
